a917e3120defbd0bfa5fb7e1ca69c3673bc1f273
Eudis35 <mondesirkoffi30@gmail.com>
2024-09-05 14:25:54 +0000

    last one

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import './Card.css'
+import React from 'react';
+import './Card.css';
+import Video from './videos/backvid.mp4';
 
 const Card = () => {
-
-    return (
-        <div>
-            <div className='card-back'>
-                
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className='card-back'>
+      <video className='video-background' autoPlay loop muted>
+        <source src={Video} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className='content'>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
